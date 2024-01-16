40123a9f88c5df49ac9803fcd684740eddde7377
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-16 20:53:21 +0200

    auth package interface + claims

---
+++ services/user/pkg/auth/auth.go
@@ -0,0 +1,20 @@
+package auth
+
+type Authenticator interface {
+	// GenerateTokens provides opportunity to encrypt access & refresh token.
+	GenerateTokens(options *GenerateTokenClaimsOptions) (string, string, error)
+
+	// GenerateRefreshToken generates refresh token
+	GenerateRefreshToken(id string) (string, error)
+
+	// ParseToken provides opportunity to decrypt access token.
+	ParseToken(accessToken string) (*ParseTokenClaimsOutput, error)
+}
+
+type GenerateTokenClaimsOptions struct {
+	UserId string `json:"sub"`
+}
+
+type ParseTokenClaimsOutput struct {
+	Sub string
+}
