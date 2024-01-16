3d98c9a99b14b4ee76a301a7293b6de488942b8b
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-16 20:55:22 +0200

    user service interface

---
+++ services/user/internal/service/service.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"context"
+	"userService/internal/domain/entity"
+)
+
+type UserService interface {
+	SignUp(ctx context.Context, input entity.SignUpInput) error
+
+	// SignIn returns a new access token and refresh token.
+	SignIn(ctx context.Context, input entity.SignInInput) (string, string, error)
+}
