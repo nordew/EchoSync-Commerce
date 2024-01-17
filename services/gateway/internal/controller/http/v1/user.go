a365a8d3a9e16266425418550f59dc3ad8533be9
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-17 15:37:31 +0200

    feat: signIn handler realization

--- services/gateway/internal/controller/http/v1/user.go
+++ services/gateway/internal/controller/http/v1/user.go
@@ -1,27 +1,57 @@
 package v1
 
 import (
+	"context"
 	"gateway/internal/controller/http/dto"
+	"github.com/gofiber/fiber/v2"
 
-	"github.com/gofiber/fiber"
 	nordew "github.com/nordew/EchoSync-protos/gen/go/user"
 )
 
-func (h *Handler) signUp(c *fiber.Ctx) {
+func (h *Handler) signUp(c *fiber.Ctx) error {
 	var input dto.SignUpRequest
 
 	if err := c.BodyParser(&input); err != nil {
 		writeInvalidJSONResponse(c)
+		return err
 	}
 
-	_, err := h.grpcClient.SignUp(c.Context(), &nordew.SignUpRequest{
+	_, err := h.grpcClient.SignUp(context.Background(), &nordew.SignUpRequest{
 		Username: input.Username,
 		Email:    input.Email,
 		Password: input.Password,
 	})
 	if err != nil {
 		writeErrorResponse(c, "internal_error", err.Error())
+		return err
 	}
 
 	c.Status(fiber.StatusCreated)
+
+	return nil
+}
+
+func (h *Handler) signIn(c *fiber.Ctx) error {
+	var input dto.SignInRequest
+
+	if err := c.BodyParser(&input); err != nil {
+		writeInvalidJSONResponse(c)
+		return err
+	}
+
+	resp, err := h.grpcClient.SignIn(context.Background(), &nordew.SignInRequest{
+		Email:    input.Email,
+		Password: input.Password,
+	})
+	if err != nil {
+		writeErrorResponse(c, "internal_error", err.Error())
+		return err
+	}
+
+	c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"access_token":  resp.AccessToken,
+		"refresh_token": resp.RefreshToken,
+	})
+
+	return nil
 }
