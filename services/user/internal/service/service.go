0ee8b7cfd407b32edae48600708105c29f2f6a0e
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-17 19:34:37 +0200

    refactor: methods input as pointer

--- services/user/internal/service/service.go
+++ services/user/internal/service/service.go
@@ -6,8 +6,8 @@ import (
 )
 
 type UserService interface {
-	SignUp(ctx context.Context, input entity.SignUpInput) error
+	SignUp(ctx context.Context, input *entity.SignUpInput) error
 
 	// SignIn returns a new access token and refresh token.
-	SignIn(ctx context.Context, input entity.SignInInput) (string, string, error)
+	SignIn(ctx context.Context, input *entity.SignInInput) (string, string, error)
 }
