6a35acfc7e4a6e0eb5682bc09f797db75412c5ac
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-17 13:30:38 +0200

    refactor: gRPC server + logging interceptor created

--- services/user/internal/app/app.go
+++ services/user/internal/app/app.go
@@ -1,9 +1,13 @@
 package app
 
 import (
+	"context"
 	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net"
 	"userService/internal/config"
 	grpcAuth "userService/internal/grpc"
@@ -26,22 +30,33 @@ func Run() error {
 		return fmt.Errorf("failed to connect to pg: %w", err)
 	}
 
-	userStoage := storage.NewUserStorage(conn, logger)
+	userStorage := storage.NewUserStorage(conn, logger)
 
 	authenticator := auth.NewAuth(cfg.JWTSignKey, logger)
 	passwordHasher := hasher.NewPasswordHasher(cfg.HasherSalt)
 
-	userService := service.NewAuthService(userStoage, authenticator, logger, passwordHasher)
+	userService := service.NewAuthService(userStorage, authenticator, logger, passwordHasher)
+
+	loggingOpts := []logging.Option{
+		logging.WithLogOnEvents(
+			logging.PayloadReceived, logging.PayloadSent,
+		),
+	}
 
 	recoveryOpts := []recovery.Option{
 		recovery.WithRecoveryHandler(func(p interface{}) (err error) {
 			logger.Error("panic occurred", p)
-			return nil
+
+			return status.Errorf(codes.Internal, "internal error")
 		}),
 	}
 
-	grpcServer := grpc.NewServer(grpc.ChainUnaryInterceptor(recovery.UnaryServerInterceptor(recoveryOpts...)))
-	grpcAuth.Register(grpcServer, userService)
+	gRPCServer := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		recovery.UnaryServerInterceptor(recoveryOpts...),
+		logging.UnaryServerInterceptor(InterceptorLogger(logger), loggingOpts...),
+	))
+
+	grpcAuth.Register(gRPCServer, userService)
 
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.GRPCPort))
 	if err != nil {
@@ -50,9 +65,15 @@ func Run() error {
 
 	logger.Info("gRPC user server is listening", "port", cfg.GRPCPort)
 
-	if err := grpcServer.Serve(l); err != nil {
+	if err := gRPCServer.Serve(l); err != nil {
 		return fmt.Errorf("failed to serve: %w", err)
 	}
 
 	return nil
+}
+
+func InterceptorLogger(l logger.Logger) logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		l.Info(msg, fields...)
+	})
 }
