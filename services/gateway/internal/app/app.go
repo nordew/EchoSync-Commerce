b4dffb9fca9d681fd8c4da35cefd7130288ce372
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-17 13:31:52 +0200

    gRPC clienr, userStorage/Service, handler initialization

---
+++ services/gateway/internal/app/app.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	v1 "gateway/internal/controller/http/v1"
+	"gateway/pkg/logging"
+	nordew "github.com/nordew/EchoSync-protos/gen/go/user"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func Run() error {
+	logger := logging.NewLogger()
+
+	userServerConn, err := grpc.Dial(":50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return err
+	}
+
+	userClient := nordew.NewUserClient(userServerConn)
+
+	handler := v1.NewHandler(logger, userClient)
+
+	app := handler.Init()
+
+	if err := app.Listen(":8080"); err != nil {
+		return err
+	}
+
+	return nil
+}
