37e287ceeb3f4c4a89992f87933752a3d2c7e467
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-17 13:32:24 +0200

    SignUp/In request inputs

---
+++ services/gateway/internal/controller/http/dto/user.go
@@ -0,0 +1,12 @@
+package dto
+
+type SignUpRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type SignInRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
