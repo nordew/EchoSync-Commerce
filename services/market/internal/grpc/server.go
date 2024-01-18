90977d6a80b8858d22507e05a462997fa7118e38
Roman Horishnyi <roman@dynamic-005-005-005-014.5.5.pool.telefonica.de>
2024-01-18 16:49:53 +0200

    feat: CreateStore handler realization

---
+++ services/market/internal/grpc/server.go
@@ -0,0 +1,57 @@
+package grpcStore
+
+import (
+	"context"
+	"github.com/google/uuid"
+	nordew "github.com/nordew/EchoSync-protos/gen/go/market"
+	"google.golang.org/grpc"
+	"marketService/internal/domain/entity"
+	"marketService/internal/services"
+	"marketService/pkg/logger"
+	"time"
+)
+
+type grpcServer struct {
+	nordew.UnimplementedMarketServiceServer
+
+	storeService services.StoreService
+
+	logger logger.Logger
+}
+
+func Register(s *grpc.Server, logger logger.Logger) {
+	nordew.RegisterMarketServiceServer(s, &grpcServer{
+		logger: logger,
+	})
+}
+
+func NewStoreService(storeService services.StoreService, logger logger.Logger) *grpcServer {
+	return &grpcServer{
+		storeService: storeService,
+		logger:       logger,
+	}
+}
+
+func (s *grpcServer) CreateStore(ctx context.Context, req *nordew.CreateMarketRequest) (*nordew.Empty, error) {
+	const op = "grpcServer.CreateStore"
+
+	parsedUUID, err := uuid.Parse(req.OwnerId)
+	if err != nil {
+		s.logger.Error(op, "failed to parse UUID", err.Error())
+		return nil, err
+	}
+
+	store := &entity.Store{
+		Name:        req.GetName(),
+		OwnerUserID: parsedUUID,
+		CreatedAt:   time.Now(),
+	}
+
+	err = s.storeService.Create(ctx, store)
+	if err != nil {
+		s.logger.Error(op, "failed to create store", err.Error())
+		return nil, err
+	}
+
+	return &nordew.Empty{}, nil
+}
