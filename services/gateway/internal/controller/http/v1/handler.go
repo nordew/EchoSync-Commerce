0ecaad70542d903c02e433ddc42ca46671e8acfa
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-18 22:10:09 +0200

    feat: middleware and market handlers

--- services/gateway/internal/controller/http/v1/handler.go
+++ services/gateway/internal/controller/http/v1/handler.go
@@ -1,21 +1,29 @@
 package v1
 
 import (
+	"gateway/pkg/auth"
 	"gateway/pkg/logging"
 	"github.com/gofiber/fiber/v2"
-	nordew "github.com/nordew/EchoSync-protos/gen/go/user"
+
+	grpcStore "github.com/nordew/EchoSync-protos/gen/go/store"
+	grpcUser "github.com/nordew/EchoSync-protos/gen/go/user"
 )
 
 type Handler struct {
 	logger logging.Logger
 
-	grpcClient nordew.UserClient
+	grpcUserClient  grpcUser.UserClient
+	grpcStoreClient grpcStore.StoreServiceClient
+
+	auth auth.Authenticator
 }
 
-func NewHandler(logger logging.Logger, grpcClient nordew.UserClient) *Handler {
+func NewHandler(logger logging.Logger, grpcUserClient grpcUser.UserClient, grpcStoreClient grpcStore.StoreServiceClient, auth auth.Authenticator) *Handler {
 	return &Handler{
-		logger:     logger,
-		grpcClient: grpcClient,
+		logger:          logger,
+		grpcUserClient:  grpcUserClient,
+		grpcStoreClient: grpcStoreClient,
+		auth:            auth,
 	}
 }
 
@@ -23,21 +31,29 @@ func (h *Handler) Init() *fiber.App {
 	app := fiber.New()
 
 	auth := app.Group("/auth")
-	auth.Post("/sign-up", h.signUp)
-	auth.Get("/sign-in", h.signIn)
+	{
+		auth.Post("/sign-up", h.signUp)
+		auth.Get("/sign-in", h.signIn)
+	}
+
+	market := app.Group("/market")
+	market.Use(h.AuthMiddleware)
+	{
+		market.Post("/store", h.createStore)
+	}
 
 	return app
 }
 
 func writeInvalidJSONResponse(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-		"error": "invalid json",
+		"error": "Invalid JSON",
 	})
 }
 
-func writeErrorResponse(c *fiber.Ctx, error, errorDescription string) error {
+func writeErrorResponse(c *fiber.Ctx, errType, errDescription string) error {
 	return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-		"error":             error,
-		"error_description": errorDescription,
+		"error":             errType,
+		"error_description": errDescription,
 	})
 }
