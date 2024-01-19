a28cdcb9a81a1676cc2577b5ff6d309f16cce50d
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-19 18:49:22 +0200

    Product: Getters, updating & delete method's added

--- services/market/internal/services/storage.go
+++ services/market/internal/services/storage.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"marketService/internal/domain/entity"
 )
 
@@ -13,4 +14,16 @@ type StoreStorage interface {
 type ProductStorage interface {
 	// Create creates a new product
 	Create(ctx context.Context, product *entity.Product) error
+
+	// GetByStoreID returns all products by store id
+	GetByStoreID(ctx context.Context, storeID uuid.UUID) ([]*entity.Product, error)
+
+	// GetByID returns product by id
+	GetByID(ctx context.Context, productID uuid.UUID) (*entity.Product, error)
+
+	// Update updates product
+	Update(ctx context.Context, product *entity.Product) error
+
+	// Delete deletes product
+	Delete(ctx context.Context, productID uuid.UUID) error
 }
