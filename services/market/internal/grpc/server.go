e7d77bced7cdec321d4105fafb5c5bdc4b745f51
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-19 22:18:00 +0200

    productService added and market handler moved

--- services/market/internal/grpc/server.go
+++ services/market/internal/grpc/server.go
@@ -1,9 +1,7 @@
 package grpcStore
 
 import (
-	"context"
 	"errors"
-	"github.com/google/uuid"
 	grpcStore "github.com/nordew/EchoSync-protos/gen/go/store"
 	"google.golang.org/grpc"
 	"marketService/internal/services"
@@ -11,38 +9,23 @@ import (
 )
 
 var (
-	ErrInvalidUUId = errors.New("invalid uuid")
+	ErrInvalidUUID = errors.New("invalid uuid")
 )
 
 type grpcServer struct {
 	grpcStore.UnimplementedStoreServiceServer
+	grpcStore.UnimplementedProductServiceServer
 
-	storeService services.StoreService
+	storeService   services.StoreService
+	productService services.ProductService
 
 	logger logger.Logger
 }
 
-func Register(s *grpc.Server, storeService services.StoreService, logger logger.Logger) {
+func Register(s *grpc.Server, storeService services.StoreService, productService services.ProductService, logger logger.Logger) {
 	grpcStore.RegisterStoreServiceServer(s, &grpcServer{
-		storeService: storeService,
-		logger:       logger,
+		storeService:   storeService,
+		productService: productService,
+		logger:         logger,
 	})
-}
-
-func (s *grpcServer) CreateStore(ctx context.Context, req *grpcStore.CreateStoreRequest) (*grpcStore.Empty, error) {
-	const op = "grpcServer.CreateStore"
-
-	parsedUUID, err := uuid.Parse(req.GetOwnerId())
-	if err != nil {
-		s.logger.Error(op, err.Error())
-		return nil, ErrInvalidUUId
-	}
-
-	err = s.storeService.Create(context.Background(), req.GetName(), parsedUUID)
-	if err != nil {
-		s.logger.Error(op, "failed to create store", err.Error())
-		return nil, err
-	}
-
-	return &grpcStore.Empty{}, nil
 }
