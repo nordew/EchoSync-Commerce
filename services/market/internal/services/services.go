34f9b911859f9a8a624ea506be4d7a46de0719fa
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-19 18:25:38 +0200

    product service and create method

--- services/market/internal/services/services.go
+++ services/market/internal/services/services.go
@@ -9,3 +9,8 @@ type StoreService interface {
 	// Create creates a new store
 	Create(ctx context.Context, name string, ownerID uuid.UUID) error
 }
+
+type ProductService interface {
+	// Create creates a new product
+	Create(ctx context.Context, storeID uuid.UUID, productName string, price, quantity int) error
+}
