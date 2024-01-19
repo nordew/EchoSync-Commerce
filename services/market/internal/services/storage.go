74b499b89323b51143e600aaa6414ff1c61a0626
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-19 18:25:58 +0200

    product storage interface and crete method

--- services/market/internal/services/storage.go
+++ services/market/internal/services/storage.go
@@ -9,3 +9,8 @@ type StoreStorage interface {
 	// Create creates a new store
 	Create(ctx context.Context, store *entity.Store) error
 }
+
+type ProductStorage interface {
+	// Create creates a new product
+	Create(ctx context.Context, product *entity.Product) error
+}
