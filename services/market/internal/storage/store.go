fe9b1321fabb019a7c69c51738dac57b02263325
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-20 21:07:13 +0200

    feat: get by id, update, delete methods

--- services/market/internal/storage/store.go
+++ services/market/internal/storage/store.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"errors"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx"
 	"marketService/internal/domain/entity"
 	"marketService/pkg/logger"
@@ -49,6 +50,45 @@ func (s *storeStorage) Create(ctx context.Context, store *entity.Store) error {
 	}
 
 	_, err = s.conn.ExecEx(ctx, "UPDATE users SET stores_active = stores_active + 1 WHERE user_id = $1", nil, store.OwnerUserID)
+	if err != nil {
+		s.logging.Error(op, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (s *storeStorage) GetByID(ctx context.Context, storeID uuid.UUID) (*entity.Store, error) {
+	const op = "storeStorage.GetByID"
+
+	store := &entity.Store{}
+
+	err := s.conn.QueryRowEx(ctx, "SELECT store_id, store_name, owner_user_id, products_count, is_active, created_at FROM stores WHERE store_id = $1", nil, storeID).
+		Scan(&store.ID, &store.Name, &store.OwnerUserID, &store.ProductsCount, &store.IsActive, &store.CreatedAt)
+	if err != nil {
+		s.logging.Error(op, err.Error())
+		return nil, err
+	}
+
+	return store, nil
+}
+
+func (s *storeStorage) Update(ctx context.Context, storeName string, storeID uuid.UUID) error {
+	const op = "storeStorage.Update"
+
+	_, err := s.conn.ExecEx(ctx, "UPDATE stores SET store_name = $1 WHERE store_id = $2", nil, storeName, storeID)
+	if err != nil {
+		s.logging.Error(op, err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func (s *storeStorage) Delete(ctx context.Context, storeID uuid.UUID) error {
+	const op = "storeStorage.Delete"
+
+	_, err := s.conn.ExecEx(ctx, "DELETE FROM stores WHERE store_id = $1", nil, storeID)
 	if err != nil {
 		s.logging.Error(op, err.Error())
 		return err
