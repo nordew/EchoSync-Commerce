bb6d65cff0af58397c00f2cdefbc682a5c5ba9ec
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-20 20:58:31 +0200

    feat: added store count checking and incrementing stores active count in user table

--- services/market/internal/storage/store.go
+++ services/market/internal/storage/store.go
@@ -2,9 +2,14 @@ package storage
 
 import (
 	"context"
+	"errors"
 	"github.com/jackc/pgx"
 	"marketService/internal/domain/entity"
 	"marketService/pkg/logger"
+)
+
+var (
+	ErrMaximalStoresCountReached = errors.New("maximal stores count reached")
 )
 
 type storeStorage struct {
@@ -21,11 +26,31 @@ func NewStoreStorage(conn *pgx.Conn, logging logger.Logger) *storeStorage {
 }
 
 func (s *storeStorage) Create(ctx context.Context, store *entity.Store) error {
-	_, err := s.conn.ExecEx(ctx,
+	const op = "storeStorage.Create"
+
+	var storesCount int
+	err := s.conn.QueryRowEx(ctx, "SELECT stores_active FROM users WHERE user_id = $1", nil, store.OwnerUserID).
+		Scan(&storesCount)
+	if err != nil {
+		s.logging.Error(op, err.Error())
+		return err
+	}
+
+	if storesCount >= 3 {
+		return ErrMaximalStoresCountReached
+	}
+
+	_, err = s.conn.ExecEx(ctx,
 		"INSERT INTO stores (store_id, store_name, owner_user_id, products_count, is_active, created_at) VALUES($1, $2, $3, $4, $5, $6)",
 		nil, store.ID, store.Name, store.OwnerUserID, store.ProductsCount, store.IsActive, store.CreatedAt)
 	if err != nil {
 		s.logging.Error("failed to create store", err.Error())
+		return err
+	}
+
+	_, err = s.conn.ExecEx(ctx, "UPDATE users SET stores_active = stores_active + 1 WHERE user_id = owner_user_id", nil)
+	if err != nil {
+		s.logging.Error("failed to update user stores_active", err.Error())
 		return err
 	}
 
