b4ef6c6837603c7c5e7f6d21b8e2f31fe80a3691
Roman Horishnyi <roman@Romans-MacBook-Air.local>
2024-01-20 21:48:48 +0200

    feat: get/update/delete methods implemented

--- services/market/internal/grpc/store.go
+++ services/market/internal/grpc/store.go
@@ -1,4 +1,4 @@
-package grpcStore
+package grpcServer
 
 import (
 	"context"
@@ -24,6 +24,62 @@ func (s *grpcServer) CreateStore(ctx context.Context, req *grpcStore.CreateStore
 	return &grpcStore.Empty{}, nil
 }
 
-func (s *grpcServer) GetMarket(ctx context.Context, request *grpcStore.GetStoreRequest) (*grpcStore.GetStoreResponse, error) {
-	panic("implement me")
+func (s *grpcServer) GetMarket(ctx context.Context, req *grpcStore.GetStoreRequest) (*grpcStore.GetStoreResponse, error) {
+	const op = "grpcServer.GetMarket"
+
+	parsedUUID, err := uuid.Parse(req.GetStoreId())
+	if err != nil {
+		s.logger.Error(op, err.Error())
+		return nil, ErrInvalidUUID
+	}
+
+	market, err := s.storeService.GetByID(context.Background(), parsedUUID)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &grpcStore.GetStoreResponse{
+		Store: &grpcStore.Store{
+			StoreId: market.ID.String(),
+			Name:    market.Name,
+			OwnerId: market.OwnerUserID.String(),
+		},
+	}
+
+	return response, nil
+}
+
+func (s *grpcServer) UpdateStore(ctx context.Context, req *grpcStore.UpdateStoreRequest) (*grpcStore.Empty, error) {
+	const op = "grpcServer.UpdateStore"
+
+	parsedUUID, err := uuid.Parse(req.GetStoreId())
+	if err != nil {
+		s.logger.Error(op, err.Error())
+		return nil, ErrInvalidUUID
+	}
+
+	err = s.storeService.Update(context.Background(), req.GetName(), parsedUUID)
+	if err != nil {
+		s.logger.Error(op, "failed to update store", err.Error())
+		return nil, err
+	}
+
+	return &grpcStore.Empty{}, nil
+}
+
+func (s *grpcServer) DeleteStore(ctx context.Context, req *grpcStore.DeleteStoreRequest) (*grpcStore.Empty, error) {
+	const op = "grpcServer.DeleteStore"
+
+	parsedUUID, err := uuid.Parse(req.GetStoreId())
+	if err != nil {
+		s.logger.Error(op, err.Error())
+		return nil, ErrInvalidUUID
+	}
+
+	if err = s.storeService.Delete(context.Background(), parsedUUID); err != nil {
+		s.logger.Error(op, "failed to delete store", err.Error())
+		return nil, err
+	}
+
+	return &grpcStore.Empty{}, nil
 }
